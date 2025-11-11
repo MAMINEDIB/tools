cb121fe0770bcadc4f51901aea5121b597724e38
MAMINEDIB <aminedibsn@gmail.com>
2025-11-11 11:10:33 +0100

    Auto-generated commit

---
+++ chunk-GZPXT573.js
@@ -0,0 +1,1 @@
+var n=function(O){return O.TODO="TODO",O.IN_PROGRESS="IN_PROGRESS",O.DONE="DONE",O}(n||{}),D=function(O){return O.LOW="LOW",O.MEDIUM="MEDIUM",O.HIGH="HIGH",O}(D||{});export{n as a,D as b};
